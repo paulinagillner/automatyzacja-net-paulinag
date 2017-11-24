749ac5d40b88fd9d2db1a8be1eef735482679734
PaulinaGillner <Paulina.Gillner@luxmed.pl>
2017-11-24 08:47:18 +0100

    dodanie plików z zadania 1

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!!!");
+            string tekst = "";
+            while (tekst != "exit")
+            {
+                Console.WriteLine("Napisz 'exit' i naciśnij klawisz 'Enter', żeby zamknąć aplikację");
+                tekst = Console.ReadLine();
+            }
+        }
+    }
+}
