857d260847926e9303c4eeed11ba0631d7b7b55b
Paulina Gillner <paulina.gillner@luxmed.pl>
2017-11-28 11:26:42 +0100

    dodanie matematyka

---
+++ Zadanie3/Matematics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3
+{
+    public class Matematics
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
